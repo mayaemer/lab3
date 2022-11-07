f346a332fd9b25cc8f2274ead343590df2124ab4
mayaemer <71705519+mayaemer@users.noreply.github.com>
2022-11-07 14:18:54 +0000

    Removed test function testing commits

--- problem2/script.js
+++ problem2/script.js
@@ -3,8 +3,4 @@ let create = document.getElementById('create');
 
 rxjs.fromEvent(create, 'click').subscribe(() => console.log('Clicked!'));
 
-function testFunction(test){
-  console.log(test)
-}
 
-testFunction('hello')
