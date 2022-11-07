c53fd4297a9aca7f2360571666cede39f027f410
mayaemer <71705519+mayaemer@users.noreply.github.com>
2022-11-07 16:59:35 +0000

    Copied javascript functions from lab 1, problem 2

--- problem2/script.js
+++ problem2/script.js
@@ -28,8 +28,57 @@ function createNote(){
   document.getElementById('notetext').value = '';
 }
 
+// function to edit the note
+function editNote(noteId, title, note){
+  let noteToEdit = document.getElementById(noteId);
+  noteToEdit.innerHTML = '';
+  noteToEdit.innerHTML = `<form id="editnote">
+    <label>Edit title:</label>
+    <input type="text" id="editTitle" name="editTitle" value="` + title + `">
+    <label>Edit note:</label>
+    <input type='text' id="editText" name="editText" value="` + note + `">
+    <label for="colour">Choose a colour:</label>
+    <select id="editColour" name="editColour">
+      <option value="#b3f0ff">Blue</option>
+      <option value="#ffbb99">Peach</option>
+      <option value="#b3e6b3">Green</option>
+      <option value="#ffcce6">Pink</option>
+    </select>
+    <button onClick='saveNote("` + noteId + `")'>Save</button>
+    </form>`;
+};
+
+//function to save the inputted values to the note
+function saveNote(noteId){
+  event.preventDefault();
+  let noteid = noteId;
+  let title = document.getElementById('editTitle').value;
+  let note = document.getElementById('editText').value;
+  let colour = document.getElementById('editColour').value;
+  let div = document.getElementById(noteId);
+  div.innerHTML = `<h1>` + title + `</h1> </p>` + note + `</p> <button onClick='editNote("` + noteid + `","` + title + `","` + note + `")'>Edit</button> <button onClick='deleteNote("` + noteid + `","` + title + `")'>Delete</button>`;
+  let noteSection = document.getElementById(noteid);
+  noteSection.style.backgroundColor = colour;
+};
+
+
+//function to delete the note
+function deleteNote(noteId, title){
+  event.preventDefault();
+  let div = document.getElementById(noteId);
+  div.remove();
+  alert(title + ' successfully deleted!');
+}
+
+// function to show the note creation section
+function showCreate(){
+	document.getElementById('create').style.display ='none';
+  document.getElementById('addnote').style.display = 'block'
+}
+
+
 rxjs.fromEvent(create, 'click')
-  .subscribe(() => createNote()
+  .subscribe(() => showCreate()
 );
 
 
