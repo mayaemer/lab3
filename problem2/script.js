48cdfc20592faf94287fcfb7a2d6d0c733ee86aa
mayaemer <71705519+mayaemer@users.noreply.github.com>
2022-11-07 14:18:27 +0000

    testing commits

--- problem2/script.js
+++ problem2/script.js
@@ -2,3 +2,9 @@
 let create = document.getElementById('create');
 
 rxjs.fromEvent(create, 'click').subscribe(() => console.log('Clicked!'));
+
+function testFunction(test){
+  console.log(test)
+}
+
+testFunction('hello')
