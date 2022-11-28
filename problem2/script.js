003a5771f7c5d5d8b96b9f04e6539b4abb5f3eaa
mayaemer <71705519+mayaemer@users.noreply.github.com>
2022-11-28 10:23:25 +0000

    I forgot to commit

--- problem2/script.js
+++ problem2/script.js
@@ -1,14 +1,41 @@
-let start = document.getElementById("start");
+const { interval, timer } = rxjs;
+ 
+const start = document.getElementById('start');
+const minCount = document.getElementById('minCount');
+const secCount = document.getElementById('secCount');
 
-rxjs.fromEvent(start, "click")
-.subscribe(() => getInput());
+rxjs.fromEvent(start, 'click')
+    .subscribe(() => {
+        startTimer();
+    })
 
-function getInput(){
-    let hour = document.getElementById('hour').value;
-    let minute = document.getElementById('minute').value;
-    let second = document.getElementById('second').value;
+function startTimer(){
 
-    console.log(hour, minute, second);
+    const timerMins = document.getElementById('minute').value;
+    const timeInSecs = timerMins * 60;
+    let countdown = timeInSecs;
+    const t = timerMins * 60 * 1000;
 
 
+    const obs = interval(1000);
+ 
+    const sub = obs
+        .subscribe(x => {
+            if(x == (timeInSecs - 1)){
+                console.log('Finished')
+            }else{
+                countdown = countdown - 1;
+                displayCountdown(countdown)
+            }
+        });
+
+
+    setTimeout(() => {
+        // Unsubscribes BOTH subscription and childSubscription
+        sub.unsubscribe();
+    }, t);
+}
+
+function displayCountdown(countdown){
+    
 }
