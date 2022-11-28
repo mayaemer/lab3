1bc82cea373e7ccfdd2e3cea8b52c30d7f3a483b
mayaemer <71705519+mayaemer@users.noreply.github.com>
2022-11-28 11:31:25 +0000

    covnverted the inputted values to integers times up displays when countdown finishs

--- problem2/script.js
+++ problem2/script.js
@@ -4,6 +4,7 @@ const start = document.getElementById('start');
 const hourCount = document.getElementById('hourCount');
 const minCount = document.getElementById('minCount');
 const secCount = document.getElementById('secCount');
+const endTimer = document.getElementById('endTimer');
 
 rxjs.fromEvent(start, 'click')
     .subscribe(() => {
@@ -12,23 +13,26 @@ rxjs.fromEvent(start, 'click')
 
 function startTimer(){
 
+    endTimer.style.display = 'none';
+
     const timerHours = document.getElementById('hour').value;
     const timerMins = document.getElementById('minute').value;
     const timerSecs = document.getElementById('second').value;
 
-    const hourInSecs = timerHours * 60 * 60;
+    const hourInSecs = (timerHours * 60) * 60;
     const minInSecs = timerMins * 60;
-    timeInSecs = hourInSecs + minInSecs + timerSecs;
-    let countdown = timeInSecs;
-    const t = timerMins * 60 * 1000;
 
+    timeInSecs = parseInt(hourInSecs) + parseInt(minInSecs) + parseInt(timerSecs);
+
+    let countdown = timeInSecs;
+    const t = timeInSecs  * 1000;
 
     const obs = interval(1000);
  
     const sub = obs
         .subscribe(x => {
             if(x == (timeInSecs - 1)){
-                console.log('Finished')
+                endTimer.style.display = 'block';
             }else{
                 countdown = countdown - 1;
                 displayCountdown(countdown)
@@ -45,15 +49,15 @@ function startTimer(){
 function displayCountdown(countdown){
     if( countdown < 59)
     {
-        secCount.innerHTML = countdown;
+        secCount.innerHTML = countdown + 's';
     }else{
         // found on stack overflow https://stackoverflow.com/questions/37096367/how-to-convert-seconds-to-minutes-and-hours-in-javascript
-        let hour = Math.floor(countdown / 36000);
+        let hour = Math.floor(countdown / 3600);
         let min = Math.floor(countdown % 3600 / 60);
         let sec = Math.floor(countdown % 3600 % 60);
 
-        hourCount.innerHTML = hour;
-        minCount.innerHTML = min;
-        secCount.innerHTML = sec;
+        hourCount.innerHTML = hour + 'h';
+        minCount.innerHTML = min + 'm';
+        secCount.innerHTML = sec + 's';
     }
 }
