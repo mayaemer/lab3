519e900c46d186c4127e6accf36e6851ca4c8925
mayaemer <71705519+mayaemer@users.noreply.github.com>
2022-11-28 10:50:16 +0000

    function to convert countdown time to hours mins and secs

--- problem2/script.js
+++ problem2/script.js
@@ -1,6 +1,7 @@
 const { interval, timer } = rxjs;
  
 const start = document.getElementById('start');
+const hourCount = document.getElementById('hourCount');
 const minCount = document.getElementById('minCount');
 const secCount = document.getElementById('secCount');
 
@@ -11,8 +12,13 @@ rxjs.fromEvent(start, 'click')
 
 function startTimer(){
 
+    const timerHours = document.getElementById('hour').value;
     const timerMins = document.getElementById('minute').value;
-    const timeInSecs = timerMins * 60;
+    const timerSecs = document.getElementById('second').value;
+
+    const hourInSecs = timerHours * 60 * 60;
+    const minInSecs = timerMins * 60;
+    timeInSecs = hourInSecs + minInSecs + timerSecs;
     let countdown = timeInSecs;
     const t = timerMins * 60 * 1000;
 
@@ -36,4 +42,18 @@ function startTimer(){
     }, t);
 }
 
+function displayCountdown(countdown){
+    if( countdown < 59)
+    {
+        secCount.innerHTML = countdown;
+    }else{
+        // found on stack overflow https://stackoverflow.com/questions/37096367/how-to-convert-seconds-to-minutes-and-hours-in-javascript
+        let hour = Math.floor(countdown / 36000);
+        let min = Math.floor(countdown % 3600 / 60);
+        let sec = Math.floor(countdown % 3600 % 60);
 
+        hourCount.innerHTML = hour;
+        minCount.innerHTML = min;
+        secCount.innerHTML = sec;
+    }
+}
